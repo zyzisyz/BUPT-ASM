b92becdf2b94d2d1197a4395e39175603b88e861
zyz <zyziszy@foxmail.com>
2018-12-08 00:12:26 +0800

    update

--- B.asm
+++ B.asm
@@ -1,19 +1,19 @@
 ;data
 DATA SEGMENT
     ;nums
-    NUMS DB -4,-3,-2,-1,0,1,2,3,4
-    COUNT equ $-buff
+    NUMS DW -1,2,0,0,1,-9
+    COUNT EQU $-NUMS
     
     ;output data
-    POSITIVE_NUM db ?
-    ZERO_NUM db ?
-    NEGATIVE_NUM db ?
+    POSITIVE_NUM DB ?
+    ZERO_NUM DB ?
+    NEGATIVE_NUM DB ?
     
     ;output strings
-    string0 db 'nums: -4,-3,-2,-1,0,1,2,3,4'
-    string1 db 'Positive number:','$'
-    string2 db 'Zero number:','$'
-    string3 db 'Negative number:','$'
+    string0 DB 'Nums is: -1,2,0,0,1,-9$'
+    string1 DB 'Positive number:','$'
+    string2 DB 'Zero number:','$'
+    string3 DB 'Negative number:','$'
 DATA ENDS
 
 
@@ -30,9 +30,91 @@ START:
     MOV AX,DATA
     MOV DS,AX
     MOV CX,COUNT
+    SHR CX,1        ;dw=2db
+    MOV DX,0
+    MOV AH,0
+    LEA BX,NUMS
+COMPERE:
+    CMP WORD PTR[BX],0
+    JGE POSITIVE_CONT
+    INC AH
+    JMP NEXT
+POSITIVE_CONT:
+    JZ ZERO_CONT
+    INC DL
+    JMP NEXT
+ZERO_CONT:
+    INC DH
+NEXT:
+    INC BX
+    INC BX
+    LOOP COMPERE
+    
+    ;POSITIVE_NUM
+    ADD DL,48
+    MOV POSITIVE_NUM,DL
+    
+    ;ZERO_NUM
+    ADD DH,48
+    MOV ZERO_NUM,DH
 
+    ;NEGATIVE_NUM
+    ADD AH,48
+    MOV NEGATIVE_NUM,AH
+    
+DISPLAY:
+    sub dx,dx       ;dx to zero
+    MOV AH,09H
+    mov dx,offset string0
+    int 21h
+    
+    ;display enter
+    mov ah,02h
+    mov dl,0dh
+    int 21h
+    mov ah,02h
+    mov dl,0ah
+    int 21h 
 
-   
+    sub dx,dx       ;dx to zero
+
+    ;POSITIVE_NUM
+    mov dx,offset string1
+	mov ax,0900h
+	int 21h
+
+	mov dl,[POSITIVE_NUM]
+	mov ax,0200h
+	int 21h
+
+    mov dl,0dh
+    int 21h
+    mov dl,0ah
+    int 21h
+	
+	mov dx,offset string2
+	mov ax,0900h
+	int 21h
+	
+	mov dl,[ZERO_NUM]
+	mov ax,0200h
+	int 21h
+
+    mov dl,0dh
+    int 21h
+    mov dl,0ah
+    int 21h
+
+    mov dx,offset string3
+	mov ax,0900h
+	int 21h
+    
+	mov ax,0200h
+	mov dl,[NEGATIVE_NUM]
+	int 21h
+	
+	mov ax,4c00h
+	int 21h
 
 CODE ENDS
     END START
