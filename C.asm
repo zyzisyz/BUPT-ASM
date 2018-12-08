24acc09e81ea0639418510ecedbc528c3ba3e6f3
zyz <zyziszy@foxmail.com>
2018-12-08 20:28:29 +0800

    add c.asm

--- C.asm
+++ C.asm
@@ -4,8 +4,8 @@ DATA SEGMENT
     MIN db '9','9'
     
     ;output strings
-    string0 DB 'Input some integers, devided with SPACE, stop with ENTER:','$'
-    string1 DB 'Input Error!:','$'
+    string0 DB 'Input some integers, devided with SPACE, show result with ENTER:','$'
+    string1 DB 'Input Error!, please input again:','$'
     string2 DB 'The Minest number is:','$'
 DATA ENDS
 
@@ -19,15 +19,14 @@ STACK ENDS
 ;code
 CODE SEGMENT
     ASSUME CS:CODE,DS:DATA,SS:STACK
-
-;display enter char process
+;display enter char
 DisplayEnter proc near
     push ax
     push dx
     mov ah,2h
     mov dl,0dh
     int 21h
-    mov   ah,2h
+    mov ah,2h
     mov dl,0ah
     int 21h
     pop dx
@@ -35,11 +34,12 @@ DisplayEnter proc near
     ret 
 DisplayEnter endp
 
-; input char to al
+;input char to al
 InputChar proc near
     mov ah,01h
     int 21h
-IntputChar endp
+    ret
+InputChar endp
 
 ;judge if al in 0-9 
 Test_Num proc near
@@ -70,29 +70,29 @@ Input_loop:
     cmp al,00h
     jz Display_Error
     mov bh,al   ;put high num in b high
-
     ;input low num
     call InputChar
     call Test_Num
     cmp al,00h
     jz Display_Error
     mov bl,al   ;put low num in b low
-    
     ;input space
     call InputChar
     cmp al,0dh
-    jz Display_Result
+    jz Compare_Last
     cmp al,' '
-    jz Compere
+    jz Compare
     jmp Display_Error
 
 Display_Error:	
+    call DisplayEnter
 	mov dx,offset string1
 	mov ax,0900h
 	int 21h
+    call DisplayEnter
 	jmp Input_loop
 
-Compere:
+Compare:
     cmp bx,word ptr MIN
     jb Change_NUM
     jmp Input_loop
@@ -101,7 +101,29 @@ Change_NUM:
     mov word ptr MIN,bx
 	jmp Input_loop
 
-Display_Result:
+Compare_Last:
+    ;for last num
+    cmp bx,word ptr MIN 
+    jb Swap
+    jmp Show_Result
 
+Swap:
+    mov word ptr min,bx
+    jmp Show_Result
+
+Show_Result:
+    mov dx,offset string2
+    ;display high
+    mov ax,0900h
+	int 21h
+	mov dl,min+1
+	mov ax,0200h
+	int 21h
+
+    ;diplay low
+	mov dl,min
+	int 21h
+	mov ax,4c00h
+	int 21h
 CODE ENDS
     END START
